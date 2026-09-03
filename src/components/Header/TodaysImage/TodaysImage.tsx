0e041a872aac326b7d917c8434f8b6c72415c9f4
EmanuelRigo <112441019+EmanuelRigo@users.noreply.github.com>
2026-09-03 17:15:00 -0300

    change

---
+++ src/components/Header/TodaysImage/TodaysImage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const TodaysImage = () => {
+  <View>
+    <Text>Today Image</Text>
+  </View>;
+};
+
+export default TodaysImage;
